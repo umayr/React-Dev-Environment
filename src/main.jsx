dd8e2ea73d06ff5f693708723231f1d340a50eda
Joseph Meli <Joemeli631@gmail.com>
2016-04-13 22:09:27 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Panel = require('./components/panel.jsx');
+var ListManager = require('./components/ListManager.jsx');
 
-ReactDOM.render(<Panel title="20" content="New followers added this month" firstLineWeight="1"/>, document.getElementById('basicPanel1'));
-ReactDOM.render(<Panel title="$1250" content="Average Montly Income"/>, document.getElementById('basicPanel2'));
-ReactDOM.render(<Panel title="$13865" content="Yearly Income Goal"/>, document.getElementById('basicPanel3'));
-ReactDOM.render(<Panel title="18" content="Paris" color="orange"  textcolor="white"/>, document.getElementById('basicPanel4'));
+ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="ToDo-List" />, document.getElementById('ToDo'));
+ReactDOM.render(<ListManager title="Christmas List" headingColor="#b31217"/>, document.getElementById('Christmas'));
