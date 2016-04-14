e0eaf6a54d28a0a8b2a4c448a1086e4c0ff74f90
Joseph Meli <Joemeli631@gmail.com>
2016-04-13 22:15:16 -0400

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,17 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
 
-
 var List = React.createClass({
-  render:function(){
-      var listItems = ingredients.map(function(item){
-          return <ListItem key={item.id} ingredient={item.text} />;
-      });
-      return (<ul>{listItems}</ul>);
-  }
+    render: function() {
+
+        var createItem = function(text, index) {
+            return <ListItem key={index + text} text={text} />;
+        };
+
+        return (<ul>{this.props.items.map(createItem)}</ul>);
+    }
 });
+
+
 
 module.exports = List;
