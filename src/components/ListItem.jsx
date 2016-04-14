b213e1091255051378860ae207e77178261b27e1
Joseph Meli <Joemeli631@gmail.com>
2016-04-13 22:12:07 -0400

    Update ListItem.jsx

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -11,15 +11,5 @@ var ListItem = React.createClass({
     }
 });
 
-=======
-var ListItem = React.createClass({
-  render: function(){
-    return(
-      <li>
-            <h4>{this.props.ingredient}</h4>
-      </li>
-    );
-  }
-});
->>>>>>> b8df889db42223c31dde7ca3853aed94bdfb8500
+
 module.exports = ListItem;
